ae15c0bc5427cb6f477815be62c50cb23fe30811
EmricB <2533198@carrefour.cegepvicto.ca>
2025-11-18 11:03:24 -0500

    commit1

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// ====================================================
+// =  Déclaration des variables globales              =
+// ====================================================
+
+const INPUT_TEXT = document.getElementById("saisie-tache");
+
+const ajouterTache = document.getElementById("boutonPlus");
+
+// ====================================================
+// =  Déclaration des événements                      =
+// ====================================================
+
+ajouterTache.addEventListener("click", newLi);
+
+INPUT_TEXT.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    newLi();
+  }
+});
+
+// ====================================================
+// =  Code qui sera exécuté au chargement de la page  =
+// ====================================================
+
+
+
+// ====================================================
+// =  Déclaration des fonctions                       =
+// ====================================================
+
+function newLi(){
+    let NouvelleTache = document.createElement("li");
+    NouvelleTache.id = "idTache";
+    NouvelleTache.innerText = INPUT_TEXT.innerText
+}
